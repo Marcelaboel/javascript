3a5d6c5340d89df15cdae31a19bef65a28c373bc
Marcelaboel <128606582+Marcelaboel@users.noreply.github.com>
2023-07-26 11:15:22 -0300

    ex008

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if(vel > 60) { //condição simples! Pq não tem o se não
+    console.log(`Você ultrapassou a velocidade permitida. Multado!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança!`)
